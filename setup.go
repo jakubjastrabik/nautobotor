b2851ad6a0f290f11f4c3fe51868d9741a58a0c7
Jakub Jastrabík <jastrabik.kubko@gmail.com>
2022-01-16 13:20:16 +0100

    Change version

--- setup.go
+++ setup.go
@@ -10,7 +10,7 @@ import (
 	"github.com/jakubjastrabik/nautobotor/ramrecords"
 )
 
-var Version = "v0.2.5"
+var Version = "v0.2.6"
 
 // init registers this plugin.
 func init() { plugin.Register("nautobotor", setup) }
