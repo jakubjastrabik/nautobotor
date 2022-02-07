4a06bcec736ff9db185fc9300a30c27a672c35fb
Jakub Jastrabík <jastrabik.kubko@gmail.com>
2022-02-07 18:20:00 +0100

    Fix all issue with api

--- setup.go
+++ setup.go
@@ -10,7 +10,7 @@ import (
 	"github.com/jakubjastrabik/nautobotor/ramrecords"
 )
 
-var Version = "v0.5"
+var Version = "v0.50.6"
 
 // init registers this plugin.
 func init() { plugin.Register("nautobotor", setup) }
