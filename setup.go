c43048dc863f9e4e7d62ae4ebe30c94d97e5f2f0
Jakub Jastrabík <jastrabik.kubko@gmail.com>
2022-02-07 16:42:53 +0100

    Try different header

--- setup.go
+++ setup.go
@@ -10,7 +10,7 @@ import (
 	"github.com/jakubjastrabik/nautobotor/ramrecords"
 )
 
-var Version = "v0.5.3"
+var Version = "v0.5.4"
 
 // init registers this plugin.
 func init() { plugin.Register("nautobotor", setup) }
